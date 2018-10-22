f2ec0addd69d64fe893ac1d67c77dba740489adc
chris <chris@192.168.0.6>
2018-10-22 22:29:56 +0200

    Changed the server and the lobby, commented the current code out

---
+++ Server/src/main/Lobby.java
@@ -0,0 +1,128 @@
+	package main;
+
+	import java.io.IOException;
+	import java.io.ObjectInputStream;
+	import java.io.ObjectOutputStream;
+	import java.io.Serializable;
+	import java.net.ServerSocket;
+	import java.net.Socket;
+	import java.net.UnknownHostException;
+	import java.util.concurrent.Semaphore;
+
+	import master.MainServer;
+
+	/**
+	 * The Class Server.
+	 */
+	public class Lobby implements Serializable {
+		private static final long serialVersionUID = -216506295654L;
+		public static final int PORT = 4445;
+		public static final int MAX_USERS = 5000;
+		public static final String HOST = "localhost";
+		public String name = "Lobby Server";
+		public static void main(String[] args) throws IOException, ClassNotFoundException {
+			Lobby s = new Lobby();
+			s.runServer();
+		}
+		public void runServer() throws IOException, ClassNotFoundException {
+			registerServer();
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			System.out.println("Server waiting for connections...");
+			while (true) {
+				Socket socket = serverSocket.accept();
+				new ServerThread(socket).start();
+			}
+		}
+		private void registerServer() throws UnknownHostException, IOException, ClassNotFoundException {
+			// Method for establishing a connection to the MainServer 
+			Socket socket = new Socket(MainServer.HOST, MainServer.PORT);
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+			objectOutputStream.writeObject(this);
+			System.out.println((String) objectInputStream.readObject());
+		}
+		public class ServerThread extends Thread {
+			public Socket socket = null;
+			ServerThread(Socket socket) {
+				this.socket = socket;
+			}
+			public void run() {
+				try {			
+					// This method is for when the client want's to connect to the lobby
+					ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+					ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+					Client joined = (Client) objectInputStream.readObject();
+					System.out.println(joined.name + " is now connected.");
+					objectOutputStream.writeObject("You joined the server.");
+					while (true) {
+						System.out.println(objectInputStream.readObject());
+					}
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+/*	
+//	numberOfPlayers = Server.getNumberOfClients();
+	
+	newDiceTest dice = new newDiceTest(numberOfPlayers);
+	
+	private boolean checkIfReady() {
+		if(playerReady == false) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+//	public Lobby() {
+//		
+//	}
+//	
+//	public Lobby(String clientName, int playerScore) {
+//		this.clientName = clientName;
+//		this.playerScore = playerScore;
+//	}
+
+	
+	// !!!!!!!!!! The following functions probably belong in a Board class, not this class !!!!!!!!!!!! //
+	public void setDiceSize() {
+		diceSize = dice.getDiceSize();
+	}
+	
+	// This should return client's name, followed by their individual score
+	public int getBoardScore() {
+		return this.playerScore;
+	}
+	
+	// Not sure what this one should do, it's taken from the class diagram
+	public int getDiceRoll() {
+		return diceRoll;
+	}
+	
+	// This function should add the dice value to player's score
+	
+	// Need help to get access to the Dice class
+	private void addDiceToScore(int diceToScore) {
+//		int diceValue = Dice.roll();
+		this.playerScore += diceToScore;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+*/
--- Server/src/master/MainServer.java
+++ Server/src/master/MainServer.java
@@ -1,13 +1,63 @@
+package master;
 
-/*import java.net.*;
+import java.net.*;
 import java.io.*;
-import java.util.*;*/
+import java.util.*;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+import main.Lobby;
+
+public class MainServer {
+
+	public static final int PORT = 4444;
+	public static final String HOST = "localhost";
+	public ArrayList<Lobby> serverList = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		new MainServer().runServer();
+	}
+
+	public void runServer() throws IOException, ClassNotFoundException {
+		
+		// Creating the server
+
+		ServerSocket serverSocket = new ServerSocket(PORT);
+		System.out.println("Main Server initiated.");
+
+		while (true) {
+
+			Socket socket = serverSocket.accept();
+
+			try {
+				
+				// Establishing the connection to the Lobby server and then adding it to its list
+				ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+				objectOutputStream.writeObject("Server created successfully.");
+				Lobby s = (Lobby) objectInputStream.readObject();
+				this.serverList.add(s);
+				System.out.println("Server \"" + s.name + "\" added to game list.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+/*
 public class Server {
 	static java.util.Date serverCreate;
 	private static int maxClients = 0;
@@ -186,3 +236,5 @@ class HandleAClient implements Runnable {
 		}
 	}
 }
+
+*/
