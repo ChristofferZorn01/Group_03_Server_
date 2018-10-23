8612de5d17c871378b3e46534cb51b386ea7c14e
Skinkelyn <asba13aau@gmail.com>
2018-10-23 10:59:30 +0200

    Some code formatting changes, minor change to dice.java roll method

--- MiniprojectPCSS/src/Dice.java
+++ MiniprojectPCSS/src/Dice.java
@@ -1,39 +1,36 @@
-// DO NOT TOUCH
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
- 
-public class Dice {
-    // Defining...
-    private int sides; // Number of sides on the die
-    public int faceValue;
- 
-    // Creating new random object, to create a random roll
-    Random random = new Random();
-   
-    public List<Dice> listWithRolls = new ArrayList<Dice>();
- 
-    // Constructor, that takes in the number of players
-    public Dice(int players) {
-        // There will always be one more side on the die than there are players
-        sides = players + 1;
-    }
- 
-    // Rolling the die
-    public void roll() {
- 
-        // Value for face. Take in sides as argument, which sets the max. 1 is min.
-        faceValue = random.nextInt(sides) + 1;
-        //return faceValue;
-        ad
-    }
-   
- 
- 
-    // String to print
-    public String toString() {
-        return "" + roll() + ". ";
- 
-    }
- 
-}
+	
+	// DO NOT TOUCH
+	import java.util.ArrayList;
+	import java.util.List;
+	import java.util.Random;
+	 
+	public class Dice {
+	    // Defining...
+	    private int sides = 6; // Number of sides on the die
+	    public int faceValue;
+	 
+	    // Creating new random object, to create a random roll
+	    Random random = new Random();
+	   
+	    public List<Dice> listWithRolls = new ArrayList<Dice>();
+	 
+	    // Constructor, that takes in the number of players
+	    public Dice(int players) {
+	        // There will always be one more side on the die than there are players
+	        sides = players + 1;
+	    }
+	 
+	    // Rolling the die
+	    public int roll() {
+	        // Value for face. Take in sides as argument, which sets the max. 1 is min.
+	    	this.faceValue = faceValue;
+	        faceValue = random.nextInt(sides) + 1;
+	        System.out.println("You rolled " + faceValue);
+	        return faceValue;
+	    }
+	 
+	    // String to print
+	    public String toString() {
+	        return roll();
+	    }
+	}
--- MiniprojectPCSS/src/Player.java
+++ MiniprojectPCSS/src/Player.java
@@ -1,23 +1,23 @@
-// DO NOT TOUCH
-public class Player {
- 
-    private String name;
-    private int totalScore = 0;
- 
-    Player(String name) {
-        this.name = name;
-    }
- 
-    String getName() {
-        return name;
-    }
- 
-    void changeScore(int roundScore) {
-        totalScore += roundScore;
-    }
- 
-    int getTotalScore() {
-        return totalScore;
-    }
- 
-}
+	
+	public class Player {
+	 
+	    private String name;
+	    private int totalScore = 0;
+	 
+	    Player(String name) {
+	        this.name = name;
+	    }
+	 
+	    String getName() {
+	        return name;
+	    }
+	 
+	    void changeScore(int roundScore) {
+	        totalScore += roundScore;
+	    }
+	 
+	    int getTotalScore() {
+	        return totalScore;
+	    }
+	 
+	}
