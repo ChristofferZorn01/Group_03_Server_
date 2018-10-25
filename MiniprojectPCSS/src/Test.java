a934450100c512c803cc5e1959390fb39be856a8
Fregnen <lbpe17@student.aau.dk>
2018-10-25 12:12:17 +0200

    Almost there

--- MiniprojectPCSS/src/Test.java
+++ MiniprojectPCSS/src/Test.java
@@ -16,41 +16,35 @@ public class Test {
 
 		System.out.println("Put in number of players:");
 		numberOfPlayers = sc.nextInt();
-		dice = new Dice(numberOfPlayers);
 		System.out.println("There are " + numberOfPlayers + " players. Press '0' to roll.");
 		int num = sc.nextInt();
 
 		while (connected) {
-			if (num == 0) {
 
-				// Adding player to a list with name "Player no. 1" and so on...
-				for (int i = 0; i < numberOfPlayers; i++) {
-					player = new Player((i + 1)); // Giving player a number, which starts at 1. Starting with 0 points.
-					listOfPlayers.add(player);// Adding the new player to the list listOfPlayers
-
-					dice.roll();
-
-					System.out.println(listOfPlayers.get(i).getName() + " rolled a " + dice.getRollsFromList(i));
-				}
+			// Adding player to a list with name "Player no. 1" and so on...
+			for (int i = 0; i < numberOfPlayers; i++) {
+				player = new Player((i + 1)); // Giving player a number, which starts at 1. Starting with 0 points.
+				listOfPlayers.add(player);// Adding the new player to the list listOfPlayers
 			}
 
-			int obj = Collections.max(dice.listWithRolls);
-
-			if (num == 0 || num == 1) {
-				
+			if (num == 0) {
+				dice = new Dice(numberOfPlayers);
 				for (int i = 0; i < numberOfPlayers; i++) {
 					dice.roll();
 					System.out.println(listOfPlayers.get(i).getName() + " rolled a " + dice.getRollsFromList(i));
 				}
 
+				
+				
+				
 				outerloop: for (int i = 0; i < numberOfPlayers; i++) {
+					int obj = Collections.max(dice.listWithRolls);
 					if (dice.getRollsFromList(i) == obj) {
 
 						for (int j = i + 1; j < numberOfPlayers; j++) {
 							if (dice.getRollsFromList(i) != dice.getRollsFromList(j) && j == (numberOfPlayers - 1)) {
 								System.out.println((i + 1) + " rullede max som den eneste. Får 1 point.");
 								listOfPlayers.get(i).addScore();
-
 							}
 
 							else if (dice.getRollsFromList(i) == dice.getRollsFromList(j)) {
@@ -63,15 +57,18 @@ public class Test {
 							}
 						}
 					}
-				} break;
+				}
+				System.out.print("\nScoring list:\n");
+				for (int i = 0; i < numberOfPlayers; i++) {
+					System.out.print(listOfPlayers.get(i).getName() + " - total score: "
+							+ listOfPlayers.get(i).getTotalScore() + "\n");
+			}
 
+			
+			} else if(num!=0) {
+				System.out.println("Invalid input.");
 			}
-			System.out.print("Scoring list:\n");
-			for (int i = 0; i < numberOfPlayers; i++) {
-				System.out.print(listOfPlayers.get(i).getName() + " - total score: "
-						+ listOfPlayers.get(i).getTotalScore() + "\n");
-			}
-			System.out.println("press '1' to roll again");
+			System.out.println("Press '0' to roll");
 			num = sc.nextInt();
 
 		}
